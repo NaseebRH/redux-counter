45ef75929a6a777d19a2de26a353aeec36bc7ccb
NaseebRH <naseeb.rh@gmail.com>
2019-02-19 15:22:50 +0100

    redux-first-project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import Counter from './counter'
+
+const reducer = (state = {count : 0}, {type}) => {
+  switch (type) {
+    case 'INCREMENT':
+      return {...state, count : state.count + 1}
+    case 'DECREMENT':
+      return {...state, count : state.count - 1}
+    default:
+      return state
+  }
+}
+
+const store =createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const currentState = store.getState();
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <Provider store={store}> <div className="App">
+     <Counter/>
+      </div></Provider>
     );
   }
 }
